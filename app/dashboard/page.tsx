f9277e10fa800ca69b31c9da8fb01b81238556d6
BakarCherkezishvili <b.cherqezishvili@gmail.com>
2025-04-15 12:51:08 +0400

    Practice: Fetch data for the <Card> components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,20 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
+import { fetchRevenue, fetchLatestInvoices,  fetchCardData,
+} from '@/app/lib/data';
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
+  const newLocal = await fetchRevenue();
+  const revenue = newLocal;
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -22,9 +32,9 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} /> 
       </div>
     </main>
   );
-}
+} 
