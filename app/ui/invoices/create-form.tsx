66827341c88896b3f2632fa6b37977b1607a9ad8
BakarCherkezishvili <b.cherqezishvili@gmail.com>
2025-04-26 15:14:23 +0400

    Updating form and atction with zod

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,8 +10,15 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice, State } from '@/app/lib/actions';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
+  return <form action={formAction}>...</form>;
+
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
