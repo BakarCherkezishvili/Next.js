70f34361b60ad0cf1455a3455e7f389d4ea2f7c2
BakarCherkezishvili <b.cherqezishvili@gmail.com>
2025-04-26 15:29:10 +0400

    Validate forms

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -35,11 +35,18 @@ export type State = {
   message?: string | null;
 };
 export async function createInvoice(prevState: State, formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
