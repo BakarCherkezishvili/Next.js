21e0a46c3ff43cdf28456a6f6988bf20cff008df
BakarCherkezishvili <b.cherqezishvili@gmail.com>
2025-04-26 15:18:53 +0400

    Updating createInvoice with prevState and Formdata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,30 +3,38 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
 import postgres from 'postgres';
+
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string ({
+  customerId: z.string({
     invalid_type_error: 'Please select a customer.',
   }),
   amount: z.coerce
-  .number()
-  .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
 
-  
+
   status: z.enum(['pending', 'paid'], {
-      invalid_type_error: 'Please select an invoice status.',
-    }),
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -72,3 +80,4 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
+
